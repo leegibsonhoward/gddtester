675d118545f2402b1750b06ac1ddcb529b97cbc8
Lee Howard <leegibsonhoward@gmail.com>
2021-02-02 15:01:46 -0800

    feat: add status code 200 to home route

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send("<h1>GDDTester</h1>");
+  res.status(200).send("<h1>GDDTester</h1>");
 });
 
 module.exports = app;
